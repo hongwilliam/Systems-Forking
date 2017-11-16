94100a4a4a8291cc968f8dcaf27fcccccc1c0984
sonalparab <sparab@stuy.edu>
2017-11-16 17:53:01 -0500

    Fixed children but random still is the same for both

--- forking.c
+++ forking.c
@@ -5,23 +5,26 @@
 
 
 int main(){
-  srand(time(NULL));
 
   printf("pre-fork \n");
   int f = -1;
+  printf("I'm the parent: %d \n\n", getpid());
   f = fork();
+  if(f)
+    f = fork();
   if (f == 0){
-    printf("I’m a child: %d \t f: %d \t parent: %d\n", getpid(), f, getppid() );
+    printf("I’m a child: %d \n", getpid());
+    srand(time(NULL));
     int r = (rand() % 16) + 5;
+    printf("Sleeping for %d secs...\n\n",r);
     sleep(r);
-    printf("Child is finished\n");
-    return r; }
+    printf("Child is finished\n\n");
+    return r;
+  }
   else{
-    printf("I’m a parent: %d \t \n", getpid() );
-    int c = fork();
     int status;
-    f = wait(&status);
-    printf("Completed child: \t PID: %d \t how long asleep: %d \t \n", c, WEXITSTATUS(status));
+    int c = wait(&status);
+    printf("Completed child: \t PID: %d \t Asleep for: %d secs \t \n", c, WEXITSTATUS(status));
     printf("Parent is finished\n");
     exit(0);
   }
