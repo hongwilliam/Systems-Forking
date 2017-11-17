ff70205f6db67272cd7ec57e9bf01e22d0e0ee2f
sonalparab <sparab@stuy.edu>
2017-11-16 19:16:26 -0500

    Diff sleep times

--- forking.c
+++ forking.c
@@ -6,17 +6,34 @@
 
 int main(){
   printf("Pre-fork: I'm the parent and here is my PID: %d \n", getpid());
-  int f = fork();
-  if(f)
+  //forking
+  int f;
+  int f2 = 0;
+  f = fork();
+  if(f){
     f = fork();
+    //variable to distinguish the second child from the first
+    f2 = f + 1;
+  }
+  //child instructions
   if (f == 0){
-    printf("I’m a child: %d \t parent PID: %d\n\n", getpid(), getppid());
-    srand(time(NULL));
-    int r = (rand() % 16) + 5;
+    printf("I’m a child: %d \t parent PID: %d\n", getpid(), getppid());
+    int r = 0;
+    //to make different sleeping times
+    if(f2){
+      srand(time(NULL) + 1);
+      r = rand() % 16 + 5;
+    }
+    else{
+      srand(time(NULL));
+      r = rand() % 16 + 5;
+    }
+    printf("Sleeping for %d secs\n\n",r);
     sleep(r);
     printf("Child is finished\n");
     return r;
   }
+  //parent instructions
   else{
     int status;
     int c = wait(&status);
